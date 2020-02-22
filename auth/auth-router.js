fc01680bf579b769295776def78a0022b4e3b10f
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-22 09:22:56 -0800

    initial commit, register/log in working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,71 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const Users = require('./user-model');
+
+const secrets = require('../config/secrets');
+
+router.post('/register', validateUser, (req, res) => {
+  const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+            .then(user => {
+                const token = genToken(user);
+                res.status(201).json({user: user, token: token});
+            })
+            .catch(err => {
+                res.status(500).json(err.message);
+            })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password} = req.body;
+
+  Users.findBy({username})
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user);
+        res.status(200).json({username: user.username, token: token})
+      } else {
+        res.status(401).json({message: "Invalid credentials"})
+      }
+    })
+    .catch( err=> {
+      res.status(500).json(err)
+    })
 });
+
+// helper functions
+
+function validateUser(req, res, next) {
+  const postData = req.body;
+  if(!postData) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!postData.username || !postData.password) {
+    res.status(400).json({ message: 'missing required text field'})
+  } else {
+    next();
+  }
+}
+
+function genToken(user) {
+  const payload = {
+      userid: user.id,
+      username: user.username
+  }
+
+  const options = { expiresIn: '1h' };
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+return token;
+}
+
 
 module.exports = router;
---
+++ auth/user-model.js
@@ -0,0 +1,27 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+    add,
+    findBy,
+    findById
+};
+
+function add(user) {
+    return db('users')
+        .insert(user)
+        .then(ids => {
+            return findById(ids[0]);
+        });
+}
+
+function findById(id) {
+    return db('users')
+      .where({ id })
+      .first();
+}
+
+function findBy(search) {
+    return db('users')
+      .select('id', 'username', 'password')
+      .where(search)
+}
